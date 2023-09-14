1aff02e2d5fd43f57434857af6a80816511ecf05
so1min <117050517+so1min@users.noreply.github.com>
2023-09-14 10:51:42 +0900

    Delete DNS/setup.sh

--- DNS/setup.sh
+++
@@ -1,8 +0,0 @@
-cp ./00-installer-config.yaml /etc/netplan/
-cp ./named.conf.options /etc/bind/
-cp ./named.conf.local /etc/bind/
-cp ./sunrinNS.com.zone /etc/bind/
-cp ./sunrinNS.com.zone.rev /etc/bind/
-cp ./sunrinWP.com.zone /etc/bind/
-cp ./sunrinWP.com.zone.rev /etc/bind/
-systemctl restart bind9
